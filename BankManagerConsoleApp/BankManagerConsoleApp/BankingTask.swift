6b468d3b02a090bd5e9286fa0043281ad8a9f85e
mint3382 <mskim0926@naver.com>
2023-07-16 22:07:15 +0900

    test: again 민트가 만든 step3

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankingTask.swift
@@ -0,0 +1,31 @@
+//
+//  Teller.swift
+//  BankManagerConsoleApp
+//
+//  Created by mint, Jusbug on 2023/07/13.
+//
+
+import Foundation
+
+enum BankingTask: CaseIterable {
+    case loan
+    case deposit
+    
+    var title: String {
+        switch self {
+        case .deposit:
+            return "예금"
+        case .loan:
+            return "대출"
+        }
+    }
+    
+    var time: Double {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,8 +6,8 @@
 
 import Foundation
 
-let mint = Teller()
-let shinHanBank = Bank(name: "신한은행", tellers: [mint])
+
+let shinHanBank = Bank(name: "신한은행", tellers: (1,2))
 
 var programManager = ProgramManager(program: shinHanBank)
 programManager.selectMenu()
