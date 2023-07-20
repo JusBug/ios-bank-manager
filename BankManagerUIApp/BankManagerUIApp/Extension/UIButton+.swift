9cbcd9216f442b20b5805e460e6fb1721320bd9a
mint3382 <mskim0926@naver.com>
2023-07-20 16:51:04 +0900

    remove: 잘못 들어간 extension 두개 삭제

--- BankManagerUIApp/BankManagerUIApp/Extension/UIButton+.swift
+++ BankManagerUIApp/BankManagerUIApp/Extension/UIButton+.swift
@@ -7,13 +7,3 @@
 
 import UIKit
 
-extension UIButton {
-    convenience init(title: String, color: UIColor, action: UIAction) {
-        self.init(frame: .zero)
-        self.setTitle(title, for: .normal)
-        self.backgroundColor = .systemBackground
-        self.setTitleColor(color, for: .normal)
-        self.addAction(action, for: .touchUpInside)
-        
-    }
-}
--- BankManagerUIApp/BankManagerUIApp/Extension/UILabel+.swift
+++ BankManagerUIApp/BankManagerUIApp/Extension/UILabel+.swift
@@ -7,10 +7,3 @@
 
 import UIKit
 
-extension UILabel {
-    convenience init(text: String, textColor: UIColor) {
-        self.init(frame: .zero)
-        self.text = text
-        self.textColor = textColor
-    }
-}
