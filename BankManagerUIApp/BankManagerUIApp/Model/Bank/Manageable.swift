f7ecc2642bb5e67e9db6175332f9c630e8961962
JusBug <pksgwo123@gmail.com>
2023-07-20 16:55:21 +0900

    feat: UIApp에 Manageable 프로토콜 구현

---
+++ BankManagerUIApp/BankManagerUIApp/Model/Bank/Manageable.swift
@@ -0,0 +1,23 @@
+//
+//  Manageable.swift
+//  BankManagerUIApp
+//
+//  Created by 박종화 on 2023/07/20.
+//
+
+import Foundation
+
+protocol Manageable {
+    var name: String { get }
+    var group: DispatchGroup { get }
+    
+    func open()
+}
+
+extension Manageable {
+    func sortNext(to: @escaping () -> Void) {
+        group.notify(queue: .global()) {
+            to()
+        }
+    }
+}
